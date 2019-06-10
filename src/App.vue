4d4cc0eb7bbc8964279d2a953b52272fc0f8e526
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-06-10 20:29:33 -0300

    :lipstick: Novo template

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-toolbar>
+  <VApp>
+    <VToolbar app>
+      <VToolbarTitle class="headline text-uppercase">
+        <span>School</span>
+        <span class="font-weight-light">API</span>
+      </VToolbarTitle>
+      <VSpacer />
+      <VBtn flat href="https://github.com/castilh0s/School-Web" target="_blank">
+        <span class="mr-2">View on GitHub</span>
+        <VIcon>mdi-open-in-new</VIcon>
+      </VBtn>
+    </VToolbar>
 
-    <v-content>
+    <VContent>
       <router-view />
-    </v-content>
-  </v-app>
+    </VContent>
+  </VApp>
 </template>
 
 <script>
