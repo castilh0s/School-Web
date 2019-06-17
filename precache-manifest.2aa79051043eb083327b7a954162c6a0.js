e7d3b0e9065bcd2c7084b59e8dfc3b86dbf6bb97
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-06-17 19:09:46 -0300

    :rocket: GH-Pages deploy

---
+++ precache-manifest.2aa79051043eb083327b7a954162c6a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/School-Web/robots.txt"
+  },
+  {
+    "revision": "5577ccddf70e9510f41d",
+    "url": "/School-Web/js/chunk-vendors.640d8517.js"
+  },
+  {
+    "revision": "06170462716a5e498468",
+    "url": "/School-Web/js/app.68a285d9.js"
+  },
+  {
+    "revision": "5c50df4f6db945ff3de881d5c4b3e642",
+    "url": "/School-Web/index.html"
+  },
+  {
+    "revision": "5577ccddf70e9510f41d",
+    "url": "/School-Web/css/chunk-vendors.6ad0b682.css"
+  },
+  {
+    "revision": "06170462716a5e498468",
+    "url": "/School-Web/css/app.fd6db8eb.css"
+  }
+];
